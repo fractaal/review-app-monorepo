b9998d13fd71baa525b8c1c0fa3e4ace20631d60
Ben <sonoroustwo@gmail.com>
2024-09-26 21:32:02 +0800

    wip: new component

---
+++ components/Button.tsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <button className="transition-all duration-300 hover:scale-105 active:scale-90 w-fit px-4 dark:bg-slate-900 dark:text-white p-2 rounded-xl my-4 dark:hover:bg-slate-800 dark:active:bg-slate-950 bg-slate-100 hover:bg-slate-200">
+      Log in
+    </button>
+  );
+}
