128b6d223843fed307ab90a31ad0a805a5f85f84
Ben <sonoroustwo@gmail.com>
2024-09-28 11:10:41 +0800

    wip: auth logic in the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
 "use client";
 
 import Button from "@/components/Button";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { useFirebaseGoogleAuth } from "@/auth/google";
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const { signInWithGoogle } = useFirebaseGoogleAuth();
+
+  const handleGoogleLogin = async () => {
+    console.log("I was clicked!");
+    setLoading(true);
+    await signInWithGoogle();
+    setTimeout(() => setLoading(false), 2000);
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] min-h-screen font-[family-name:var(--font-geist-sans)] p-0 m-0">
       <main className="min-h-full min-w-full row-span-2 flex justify-center items-center ">
@@ -22,15 +36,21 @@ export default function Home() {
               <input
                 type="text"
                 placeholder="Username"
-                className="text-black bg-slate-100 p-4 rounded-xl dark:bg-slate-950"
+                className="text-black bg-slate-100 p-2 rounded-xl dark:bg-slate-900"
               ></input>
               <input
                 type="password"
                 placeholder="Password"
-                className="text-black bg-slate-100 p-4 rounded-xl dark:bg-slate-950"
+                className="text-black bg-slate-100 p-2 rounded-xl dark:bg-slate-900"
               ></input>
             </form>
-            <Button />
+            <div className="flex flex-nowrap justify-between">
+              <Button>Log in</Button>
+              <Button onClick={() => handleGoogleLogin()} loading={loading}>
+                <FontAwesomeIcon icon={faGoogle} className="mr-4" />
+                Log in with Google
+              </Button>
+            </div>
           </div>
         </div>
       </main>
