117d8e2b0ceab7debdda1a452107ec5af6c50a3e
Ben <benedict.jude.rocat@gmail.com>
2024-09-29 21:26:32 +0800

    wip: create a review page

---
+++ app/home/review/create/page.tsx
@@ -0,0 +1,9 @@
+import { Fragment } from "react";
+
+export default function CreateReview() {
+  return (
+    <Fragment>
+      <h2 className="text-xl font-black">Create a Review</h2>
+    </Fragment>
+  );
+}
