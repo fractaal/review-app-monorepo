134dca5b26ac58112a2fbada7e6efa8f071ef1ec
Ben <sonoroustwo@gmail.com>
2024-10-18 06:22:37 +0800

    chore: mobile app initialization

---
+++ apps/mobile-app/src/app/App.tsx
@@ -0,0 +1,194 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React, { Fragment, useRef, useState } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  Linking,
+  TextInput,
+} from 'react-native';
+import Svg, { G, Path } from 'react-native-svg';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export const App = () => {
+  const [whatsNextYCoord, setWhatsNextYCoord] = useState<number>(0);
+  const scrollViewRef = useRef<null | ScrollView>(null);
+
+  return (
+    <>
+      <StatusBar translucent={true} backgroundColor="transparent" barStyle="dark-content" />
+      <LinearGradient
+        style={{ zIndex: 500, elevation: 500, position: 'absolute', top: 0, left: 0, right: 0, height: StatusBar.currentHeight }}
+        colors={['rgba(255,255,255,1)', 'rgba(255,255,255,0)']}
+        start={{ x: 0, y: 0.5 }}
+        end={{ x: 0, y: 1 }}
+      />
+
+      <SafeAreaView
+        style={{
+          flex: 1,
+          margin: 20,
+        }}
+      >
+
+        <ScrollView
+          ref={(ref) => {
+            scrollViewRef.current = ref;
+          }}
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}
+        >
+
+          <View style={{ height: (StatusBar.currentHeight ?? 50) * 4 }} />
+
+          <View>
+            <Text style={{ ...styles.textLg, fontWeight: '900' }}>REVIEW_APP</Text>
+          </View>
+
+          <TextInput placeholder='e-mail here' style={{ borderWidth: 1, borderColor: 'black', padding: 10, borderRadius: 10 }} />
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: '#ffffff',
+  },
+  codeBlock: {
+    backgroundColor: 'rgba(55, 65, 81, 1)',
+    marginVertical: 12,
+    padding: 12,
+    borderRadius: 4,
+  },
+  monospace: {
+    color: '#ffffff',
+    fontFamily: 'Courier New',
+    marginVertical: 4,
+  },
+  comment: {
+    color: '#cccccc',
+  },
+  marginBottomSm: {
+    marginBottom: 6,
+  },
+  marginBottomMd: {
+    marginBottom: 18,
+  },
+  marginBottomLg: {
+    marginBottom: 24,
+  },
+  textLight: {
+    fontWeight: '300',
+  },
+  textBold: {
+    fontWeight: '500',
+  },
+  textCenter: {
+    textAlign: 'center',
+  },
+  text2XS: {
+    fontSize: 12,
+  },
+  textXS: {
+    fontSize: 14,
+  },
+  textSm: {
+    fontSize: 16,
+  },
+  textMd: {
+    fontSize: 18,
+  },
+  textLg: {
+    fontSize: 24,
+  },
+  textXL: {
+    fontSize: 48,
+  },
+  textContainer: {
+    marginVertical: 12,
+  },
+  textSubtle: {
+    color: '#6b7280',
+  },
+  section: {
+    marginVertical: 12,
+    marginHorizontal: 12,
+  },
+  shadowBox: {
+    backgroundColor: 'white',
+    borderRadius: 24,
+    shadowColor: 'black',
+    shadowOpacity: 0.15,
+    shadowOffset: {
+      width: 1,
+      height: 4,
+    },
+    shadowRadius: 12,
+    padding: 24,
+    marginBottom: 24,
+  },
+  listItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  listItemTextContainer: {
+    marginLeft: 12,
+    flex: 1,
+  },
+  appTitleText: {
+    paddingTop: 12,
+    fontWeight: '500',
+  },
+  hero: {
+    borderRadius: 12,
+    backgroundColor: '#143055',
+    padding: 36,
+    marginBottom: 24,
+  },
+  heroTitle: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  heroTitleText: {
+    color: '#ffffff',
+    marginLeft: 12,
+  },
+  heroText: {
+    color: '#ffffff',
+    marginVertical: 12,
+  },
+
+  connectToCloudButton: {
+    backgroundColor: 'rgba(20, 48, 85, 1)',
+    paddingVertical: 10,
+    borderRadius: 8,
+    marginTop: 16,
+    width: '50%',
+  },
+
+  connectToCloudButtonText: {
+    color: '#ffffff',
+  },
+  whatsNextButton: {
+    backgroundColor: '#ffffff',
+    paddingVertical: 16,
+    borderRadius: 8,
+    width: '50%',
+    marginTop: 24,
+  },
+  learning: {
+    marginVertical: 12,
+  },
+  love: {
+    marginTop: 12,
+    justifyContent: 'center',
+  },
+});
+
+export default App;
